233e9f8fb8e437160847f4574c8d930c03d4322b
kqarlos <kqarlos@hotmail.com>
2021-02-03 01:49:20 -0800

    adds list of result buttons

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const searchEl = document.getElementById("search");
 
 function getAutoComplete(keyword) {
     if (keyword.length < 5) {
-        console.log("too short")
         return false;
     }
     let key = "rMy1RF6fsAcJ66aNUB7kpfTNQIGb1-gAzujZ8NcCmfmWoj6hjQfbB4Q8CfDEzfZLhUCqQLfAvPOnecKX9FKaDdQBSL33mhu0SZ6j7__472iB89ZAqG9Ku_G0y0YaYHYx";
@@ -19,28 +18,39 @@ function getAutoComplete(keyword) {
 
 }
 
-function getBussinesses(keyword) {
+function getBusinesses(keyword) {
     console.log(keyword);
     let key = "rMy1RF6fsAcJ66aNUB7kpfTNQIGb1-gAzujZ8NcCmfmWoj6hjQfbB4Q8CfDEzfZLhUCqQLfAvPOnecKX9FKaDdQBSL33mhu0SZ6j7__472iB89ZAqG9Ku_G0y0YaYHYx";
     $.ajax({
-        url: `https://api.yelp.com/v3/businesses/search?term=${keyword}&latitude=37.786882&longitude=-122.399972&radius=10000&limit=48`,
+        url: `https://api.yelp.com/v3/businesses/search?term=${keyword}&latitude=37.786882&longitude=-122.399972&radius=10000&limit=15`,
         method: "GET",
         headers: {
-        authorization: `Bearer ${key}`
-    }
+            authorization: `Bearer ${key}`
+        }
     }).then(function (result) {
-        console.log("search", result)
+        console.log("search", result.businesses)
+        renderResults(result.businesses);
     })
+}
+
+function renderResults(businesses) {
+    businesses.forEach(element => {
+        let button = $("<button>");
+        button.addClass("list-group-item list-group-item-action result-button");
+        button.attr("type", "button");
+        button.text(element.name)
+        $("#results").append(button);
+    });
 }
 
 
 inputEl.addEventListener("keypress", (e) => {
-    getAutoComplete(e.target.value);
+    // getAutoComplete(e.target.value);
 });
 
 searchEl.addEventListener("click", (e) => {
     e.preventDefault();
-    getBussinesses(inputEl.value);
+    getBusinesses(inputEl.value);
 })
 
 $(() => {
